d29519682648fb300b90ce54782851617c70aed6
Ilham Ramadhani Saputro <dkijakartaramasaputro@gmail.com>
2025-04-28 09:57:18 +0700

    import project

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public int Value;
+    public Node Node;
+    public Block MergingBlock;
+    public bool Merging;
+    public Vector2 Pos => transform.position;
+    [SerializeField] private SpriteRenderer render;
+    [SerializeField] private TextMeshPro text;
+    public void Init(BlockType type){
+        Value = type.Value;
+        render.color = type.Color;
+        text.text = type.Value.ToString();
+    }
+
+    public void SetBlock(Node node){
+        if(Node != null) Node.OccupiedBlock = null;
+        Node = node;
+        Node.OccupiedBlock = this;
+    }
+    public void MergeBlock(Block blockToMergeWith){
+        MergingBlock = blockToMergeWith;
+
+        Node.OccupiedBlock = null;
+
+        MergingBlock.Merging = true;
+    }
+
+    public bool CanMerge(int value) => value == Value && !Merging && MergingBlock == null;
+}
